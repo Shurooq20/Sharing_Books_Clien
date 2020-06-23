1986ba77435d61341669175d99723c565162e899
davidko604 <davidko604@gmail.com>
2020-06-22 21:17:36 -0700

    Fix signout ui

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,32 +1,40 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React from 'react'
+import { NavLink } from 'react-router-dom'
 import Logo from '../../Iamges/logo_books_1.png'
 
 function NavBar(props) {
-
-  const { currentUser, signOut } = props;
+  const { currentUser, signOut } = props
 
   console.log(signOut, 'signout')
-  
-  return(
-    <div className='nav_bar'> 
-     <div className='nav_img'> <img src={ Logo } className='logo'></img>  </div>
-    <nav> 
-      <NavLink to='/categories'>Categories</NavLink>
-      &nbsp;  &nbsp;
-      <NavLink to='/categories/new'>New Category</NavLink>
-      &nbsp;  &nbsp;
-      <NavLink to='/users/new'>Create User</NavLink>
-      &nbsp;  &nbsp;
-      &nbsp;  &nbsp;
-      { !currentUser && <NavLink to='/sign_in'>Sign In</NavLink> }
-      { currentUser && <span>{currentUser.first_name}</span>}
-      &nbsp;  &nbsp;
-      <NavLink  to="/" onClick={signOut}>Sign Out</NavLink>
-    </nav>
+
+  return (
+    <div className='nav_bar'>
+      <div className='nav_img'>
+        {' '}
+        <img src={Logo} className='logo'></img>{' '}
+      </div>
+      <nav>
+        <NavLink to='/categories'>Categories</NavLink>
+        &nbsp; &nbsp;
+        <NavLink to='/categories/new'>New Category</NavLink>
+        &nbsp; &nbsp;
+        <NavLink to='/users/new'>Create User</NavLink>
+        &nbsp; &nbsp; &nbsp; &nbsp;
+        {currentUser ? (
+          <>
+            <span>{currentUser.first_name}</span>
+            <NavLink to='/' onClick={signOut}>
+              Sign Out
+            </NavLink>
+          </>
+        ) : (
+          <>
+            <NavLink to='/sign_in'>Sign In</NavLink>
+          </>
+        )}
+      </nav>
     </div>
   )
 }
-
 
 export default NavBar
